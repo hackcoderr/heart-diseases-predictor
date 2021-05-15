263cb0e240502f8bcc5f839e41ec839bff401e19
hackcoderr <sachinkumarkashyap9025@gmail.com>
2021-05-15 13:43:11 +0000

    some templates added for instnce(master+slave)

--- terraform/aws/aws.tf
+++ terraform/aws/aws.tf
@@ -89,7 +89,7 @@ resource "aws_security_group" "SG" {
 
 }
 
-
+/*
 # launching instance 
 resource "aws_instance" "AWS-HDP-instance" {
   ami    = "ami-0a9d27a9f4f5c0efc"
@@ -104,3 +104,83 @@ resource "aws_instance" "AWS-HDP-instance" {
   }
 
 }
+
+
+resource "aws_instance" "Ansible_Controller_Node" {
+  ami           = "ami-0a9d27a9f4f5c0efc"
+  instance_type = "t2.micro"
+  subnet_id = "${aws_subnet.subnet-1a.id}"
+  vpc_security_group_ids = ["${aws_security_group.SG.id}"]
+  key_name = "key"
+ tags ={
+    Environment = "${var.environment_tag}"
+    Name= "Ansible_Controller_Node"
+  }
+
+}
+*/
+
+resource "aws_instance" "K8S_Master_Node" {
+  ami           = "ami-04bde106886a53080"
+  instance_type = "t2.medium"
+  subnet_id = "${aws_subnet.subnet-1a.id}"
+  vpc_security_group_ids = ["${aws_security_group.SG.id}"]
+  key_name = "key"
+ tags ={
+    Environment = "${var.environment_tag}"
+    Name= "K8S_Master_Node"
+  }
+
+}
+resource "aws_instance" "K8S_Slave1_Node" {
+  ami           = "ami-04bde106886a53080"
+  instance_type = "t2.medium"
+  subnet_id = "${aws_subnet.subnet-1a.id}"
+  vpc_security_group_ids = ["${aws_security_group.SG.id}"]
+  key_name = "key"
+ tags ={
+    Environment = "${var.environment_tag}"
+    Name= "K8S_Slave1_Node"
+  }
+
+}
+resource "aws_instance" "K8S_Slave2_Node" {
+  ami           = "ami-04bde106886a53080"
+  instance_type = "t2.medium"
+  subnet_id = "${aws_subnet.subnet-1a.id}"
+  vpc_security_group_ids = ["${aws_security_group.SG.id}"]
+  key_name = "key"
+ tags ={
+    Environment = "${var.environment_tag}"
+    Name= "K8S_Slave2_Node"
+  }
+
+}
+resource "aws_instance" "JenkinsNode" {
+  ami           = "ami-0a9d27a9f4f5c0efc"
+  instance_type = "t2.micro"
+  subnet_id = "${aws_subnet.subnet-1a.id}"
+  vpc_security_group_ids = ["${aws_security_group.SG.id}"]
+  key_name = "key"
+ tags ={
+    Environment = "${var.environment_tag}"
+    Name= "JenkinsNode"
+  }
+
+}
+
+/*
+resource "aws_instance" "DockerNode" {
+  ami           = "ami-0a9d27a9f4f5c0efc"
+  instance_type = "t2.micro"
+  subnet_id = "${aws_subnet.subnet-1a.id}"
+  vpc_security_group_ids = ["${aws_security_group.SG.id}"]
+  key_name = "key"
+ tags ={
+    Environment = "${var.environment_tag}"
+    Name= "DockerNode"
+  }
+
+}
+
+*/
